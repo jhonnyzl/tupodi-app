1e4bee558148e03035b72d5dad9e234c442d3871
Jhonny <zeisel.2409@gmail.com>
2021-05-13 13:38:25 -0430

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,77 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Geysaneth Gonzalez',
+            category: 'Alvarado',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Bella',
+            rating: 5,
+            numReviews: 10,
+            description: 'high quality product',
+
+        },
+        {
+            _id: '2',
+            name: 'Geysaneth Gonzalez',
+            category: 'Alvarado',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Bella',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+            
+        },
+        {
+            _id: '3',
+            name: 'Geysaneth Gonzalez',
+            category: 'Alvarado',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Bella',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+            
+        },
+        {
+            _id: '4',
+            name: 'Geysaneth Gonzalez',
+            category: 'Alvarado',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Bella',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+            
+        },
+        {
+            _id: '5',
+            name: 'Geysaneth Gonzalez',
+            category: 'Alvarado',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Bella',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+            
+        },
+        {
+            _id: '6',
+            name: 'Geysaneth Gonzalez',
+            category: 'Alvarado',
+            image: '/images/p1.jpg',
+            price: 999,
+            brand: 'Bella',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+            
+        },
+    ],
+};
+export default data;
